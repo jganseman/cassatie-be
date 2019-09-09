e5a280c1fbb5d75c1f570549020caa90d3a8693a
Joachim Ganseman <joachim.ganseman@smals.be>
2019-09-09 14:36:32 +0200

    first commit of download script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cassatie-be
 Dutch language NLP dataset for legaltech applications
+
+## Introduction
+
+## Requirements
+These scripts rely on a few dependencies to do scraping. If you use Conda for your Python environment, first run:
+``` conda install selenium ```
+``` conda install -c conda-forge geckodriver ```
+If you don't use conda, check the websites of these projects for appropriate installation instructions.
+
+You need to have Firefox installed
+
+## Running the project
+1. Clone this repository
+2. Navigate to its main directory using the terminal
+3. Run ```./cassweb_download.py``` --> Firefox will launch and download a bunch of pdf files into a subfolder named ```cassweb```.
---
+++ cassweb_download.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+# author: Joachim Ganseman
+
+### Download cassatie documents from their website
+# first run "conda install selenium"
+# first run "conda install -c conda-forge geckodriver"
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from os import path, makedirs, listdir, rename
+from urllib import request, error
+
+dataroot = os.getcwd()
+dataset = "cassweb"
+target_dir = path.join(dataroot, dataset)
+
+mainurl = "https://justitie.belgium.be/nl/rechterlijke_orde/hoven_en_rechtbanken/hof_van_cassatie/documenten/arresten_van_cassatie"
+
+
+# Launch browser and navigate to website
+browser = webdriver.Firefox()
+browser.implicitly_wait(30)  # wait for up to 30 seconds before throwing element not found errs
+browser.get(mainurl)
+elems = browser.find_elements_by_tag_name('a')
+
+# extract all data as text NOW, because the browser object and its elements get invalidated once the browser connection is closed
+hrefs = [ elem.get_attribute("href") for elem in elems ]
+
+# create destination directory if needed
+if not path.exists(target_dir):
+    print("Created directory ", target_dir)
+    makedirs(target_dir)
+ 
+# download pdf files
+for myhref in hrefs:
+    #myhref = mylink.get_attribute("href")
+    if "pdf" in myhref:
+        print(myhref)
+        filename = path.basename(myhref)
+        if not path.exists(path.join(target_dir, filename)):
+            try:
+                pdfFile = request.urlopen(myhref)
+                with open(path.join(target_dir, filename), 'wb') as file:
+                    file.write(pdfFile.read())
+                    file.close()
+                print ("Saved ", path.join(target_dir, filename))
+            except error.URLError as e:
+                print("error: ", e.reason)
+            except error.HTTPError as e:
+                print("error: ", e.reason) 
+
+# no more need for the browser anymore
+browser.quit()
+
+# do some cleanup of the filenames
+for myfile in listdir(target_dir):
+    newname = myfile.replace("%20", "-")
+    newname = newname.replace("--", "-")
+    newname = newname.replace("%5B", "[")
+    newname = newname.replace("%5D", "]")
+    
+    # currently not doing the following, to keep some naming difference between the subcollections in this dataset:
+    # newname = newname.replace("AC", "ac")
+    # newname = newname.replace("_", "-")
+    
+    if (newname != myfile):
+        print(myfile, " --> ", newname)
+        rename(path.join(target_dir, myfile), path.join(target_dir, newname))
+
+# end
